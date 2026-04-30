1b2907f50c832126cbdb6ceb8a4df93cdb570927
wxn0brP <toja.filiper@gmail.com>
2026-04-30 17:45:50 +0200

    fix: import json5

--- src/format.ts
+++ src/format.ts
@@ -22,7 +22,10 @@ export const format: Record<string, Format> = {
             if (json5) return;
             // @ts-ignore
             if (typeof Bun !== "undefined" && Bun?.JSON5 && !process?.env?.VALTHERA_DIR_DISABLE_BUN) json5 = Bun.JSON5;
-            else json5 = await import("json5");
+            else {
+                const imp = await import("json5");
+                json5 = imp.default;
+            }
         }
     },
 }
