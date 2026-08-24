68ed178a05c18aa4735e391b3007932a16a927ab
wxn0brP <viviennevaltheris@gmail.com>
2026-08-24 13:52:45 +0200

    refactor: createFileActions -> createFileAdapter

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,15 @@ import { DbDirOpts } from "./types";
 export * from "./action";
 export * from "./file";
 
-export function createFileActions(folder: string, options: DbDirOpts = {}) {
+export function createFileAdapter(folder: string, options: DbDirOpts = {}) {
 	return new FileActions(folder, options, vFileCpu);
 }
 
+/**
+ * @deprecated Use {@link createFileAdapter}
+ */
+export const createFileActions = createFileAdapter;
+
 export const DYNAMIC = {
-	dir: createFileActions,
+	dir: createFileAdapter,
 };
